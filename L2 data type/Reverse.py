5ec81a684e57e383be532570e1548fd6fee7deda
racha <rachaelboukili@gmail.com>
2025-10-18 13:54:45 +0200

    HI

---
+++ L2 data type/Reverse.py
@@ -0,0 +1,4 @@
+Text = input("Enter a string: ")
+
+revText = Text[::-1]
+print("Reverse of given string is:",revText)
---
+++ L2 data type/datatype.py
@@ -0,0 +1,21 @@
+a = 67
+print("Type of a:", type(a))
+b = 6.7
+print("Type of b:", type(b))
+c = "hey"
+print("Type of c:",type (c))
+d = False
+print("Type of d:", type (d))
+
+# Typecasting
+a = str(a)
+print(a)
+print("Type of ater casting: ",type(a))
+
+b = int(b)
+print(b)
+print("Type of b after casting: ",type(b))
+
+a = float(a)
+print(a)
+print("Type oof a after casting: ",type(a))
