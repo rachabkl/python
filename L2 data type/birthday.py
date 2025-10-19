9e505054ecd664ffc1c8338a9def56caaacaa7b4
racha <rachaelboukili@gmail.com>
2025-10-19 13:46:49 +0200

    hi

---
+++ L2 data type/birthday.py
@@ -0,0 +1,20 @@
+friend1_name = "Kyle"
+friend1_birthday = "10 March"
+
+friend2_name = "Hudson"
+friend2_birthday = "22 April"
+
+friend3_name = "Paislee"
+friend3_birthday = "15 June"
+
+friend4_name = "Embreigh"
+friend4_birthday = "5 December"
+
+print(friend1_name, ":",friend1_birthday)
+print(friend2_name, ":",friend2_birthday)
+print(friend3_name, ":",friend3_birthday)
+print(friend4_name, ":",friend4_birthday)
+
+
+
+
