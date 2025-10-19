7b9a6a90016fc033a37e1f38bdbd1954757e4194
racha <rachaelboukili@gmail.com>
2025-10-19 17:46:32 +0200

    HI

---
+++ L3 operators/1.py
@@ -0,0 +1,12 @@
+tree1 = 98
+tree2 = 67
+tree3 = 41
+tree4 = 56
+tree5 = 23
+
+# Finding the total number of trees
+sum = tree1+tree2+tree4+tree5 
+print("The sum of all the 5 trees is :",sum)
+
+average=sum/5
+print("The average of all the 5 trees is :",average)
