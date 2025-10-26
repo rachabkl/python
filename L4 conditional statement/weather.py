0b5fd5e15c8091b13178804fa684ad077bf0cfa9
racha <rachaelboukili@gmail.com>
2025-10-26 15:12:46 +0100

    HI

---
+++ L4 conditional statement/weather.py
@@ -0,0 +1,8 @@
+# Program: Decide what to wear based on the weather 
+
+temperature = float(input("Enter the temperature in Celcius: "))
+
+if temperature >= 20:
+    print("It's warm outside! Wear light clothing.")
+else:
+    print("It's cold outside! Wear warm clothing.")
