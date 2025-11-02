3603f730c58f22c25f340c22ad99e8b1867ca2d2
Racha <rachaelboukili@gmail.com>
2025-11-02 14:30:43 +0100

    hi

---
+++ L6 operators3/operators3project.py
@@ -0,0 +1,11 @@
+# Program that displays the ASCII value of a given character
+
+# Ask the user to enter a character
+char = input("Enter a character: ")
+
+# Get the ASCII value using the ord() function
+ascii_value = ord(char)
+
+# Display the ASCII value
+print(f"The ASCII value of '{char}' is {ascii_value}.")
+
