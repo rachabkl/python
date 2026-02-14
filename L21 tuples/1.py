2f7d7da12d1b40c17563d019ebbae2fb66031cbf
Racha <rachaelboukili@gmail.com>
2026-02-14 17:37:24 +0100

    hi

---
+++ L21 tuples/1.py
@@ -0,0 +1,13 @@
+tu = ("tuple",False,2,3,6)
+print(tu)
+#Merging tuplex together
+tu = tu + (10,)
+print(tu)
+
+t = (2,4,5,6,7,8,9,45,4,33,45)
+print(t.count(4))
+_slice = t[3:-1]
+print(_slice)
+
+_slice = t[:6]
+print(_slice)
