3a50b06e2b9a7460943ca8a2f351854a68036da4
Racha <rachaelboukili@gmail.com>
2026-04-12 16:02:00 +0100

    hi

---
+++ L23 sets and arrays/project.py
@@ -0,0 +1,13 @@
+# A
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+
+result_a = set1.symmetric_difference(set2)
+print("A:", result_a)
+
+# B
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
+
+result_b = set1.symmetric_difference(set2)
+print("B:", result_b)
